6688501e8bb526185f682f51ab5f511d23ed90e4
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-15 10:02:25 +0530

    Create Write a Code to check whether one string is a rotation of another.cpp

---
+++ String/Write a Code to check whether one string is a rotation of another.cpp
@@ -0,0 +1,29 @@
+//Problem Link:- https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/
+//Time Complexity:- O(N+M)
+bool checkForRotation(string s,string goal){
+	if(s.size()!=goal.size()){
+		return false;
+	}
+	
+	queue<char> q1;
+	for(int i=0;i<s.size();i++){
+		q1.push(s[i]);
+	}
+
+	queue<char> q2;
+	for(int i=0;i<s.size();i++){
+		q2.push(goal[i]);
+	}
+
+	int k=goal.size();
+
+	while(k--){
+		char ch=q2.front();
+		q2.pop();
+		q2.push(ch);
+		if(q2==q1){
+			return true;
+		}
+	}
+	return false;
+}
