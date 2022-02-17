f56f6a827dd0950c574ca1ed698569a51a91574a
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-17 19:28:04 +0530

    Create Word Break.cpp

---
+++ String/Word Break.cpp
@@ -0,0 +1,43 @@
+//Problem Link:- https://practice.geeksforgeeks.org/problems/word-break1352/1#
+//Time Complexity:- O(n*m)
+//User function template for C++
+
+// A : given string to search
+// B : vector of available strings
+
+class Solution
+{
+public:
+    unordered_map<string,int> dp;
+    int solve(string s,vector<string> b){
+        int sz=s.length();
+        if(sz==0){
+            return 1;
+        }
+        if(dp[s]!=0){
+            return dp[s];
+        }
+        for(int i=1;i<=sz;i++){
+            int f=0;
+            string ss=s.substr(0,i);
+            for(int j=0;j<b.size();j++){
+                if(ss.compare(b[j])==0){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==1 and solve(s.substr(i,sz-i),b)==1){
+                return dp[s]=1;
+            }
+        }
+        return dp[s]=-1;
+    }
+    int wordBreak(string A, vector<string> &B) {
+        //code here
+        int x=solve(A,B);
+        if(x==1){
+            return 1;
+        }
+        return 0;
+    }
+};
