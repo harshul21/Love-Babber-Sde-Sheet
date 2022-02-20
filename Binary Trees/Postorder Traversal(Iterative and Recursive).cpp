dbfc676993a4153a9fd6daf3ad87c6e9407a6a1a
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-20 14:27:31 +0530

    Create Postorder Traversal(Iterative and Recursive).cpp

---
+++ Binary Trees/Postorder Traversal(Iterative and Recursive).cpp
@@ -0,0 +1,31 @@
+//Problem Link:- https://practice.geeksforgeeks.org/problems/postorder-traversal/1#
+//Time Complexity:- O(N)
+
+//Recursive
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}; */
+
+//Function to return a list containing the postorder traversal of the tree.
+void dfs(Node* root,vector<int> &ans){
+    if(root==NULL){
+        return;
+    }
+    
+    dfs(root->left,ans);
+    dfs(root->right,ans);
+    ans.push_back(root->data);
+}
+vector <int> postOrder(Node* root)
+{
+    vector<int> ans;
+    dfs(root,ans);
+    return ans;
+}
+
+//Iterative
