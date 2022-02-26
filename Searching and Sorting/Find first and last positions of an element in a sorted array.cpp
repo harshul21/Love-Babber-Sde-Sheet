f5719cf67c704114ccaa7b3144b3af468a473cdc
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-02-26 23:02:45 +0530

    Create Find first and last positions of an element in a sorted array.cpp

---
+++ Searching and Sorting/Find first and last positions of an element in a sorted array.cpp
@@ -0,0 +1,60 @@
+//Problem Link:- https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1
+//Time Complexity:- O(n*logn)
+int fo(int arr[],int n,int x)
+{
+    int res=-1;
+    int high=n-1;
+    int low=0;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid]>x)
+        {
+            high=mid-1;
+        }
+        else if(arr[mid]<x)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            res=mid;
+            high=mid-1;
+        }
+    }
+    return res;
+}
+int lo(int arr[],int n,int x)
+{
+    int res=-1;
+    int high=n-1;
+    int low=0;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid]>x)
+        {
+            high=mid-1;
+        }
+        else if(arr[mid]<x)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            res=mid;
+            low=mid+1;
+        }
+    }
+    return res;
+}
+vector<int> find(int arr[], int n , int x )
+{
+    vector<int> v;
+    int f=fo(arr,n,x);
+    int l=lo(arr,n,x);
+    
+    v.push_back(f);
+    v.push_back(l);
+    return v;
+}
