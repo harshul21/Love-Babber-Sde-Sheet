c2deb497795ff7ee6b806dbb80fe80833dc23cae
harshulgarg <56495746+harshul21@users.noreply.github.com>
2022-05-25 12:59:57 +0530

    Create DFS of Graph.cpp

---
+++ Graph/DFS of Graph.cpp
@@ -0,0 +1,30 @@
+//Problem Link:- https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1/#
+//Code:-
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> vis(V,0);
+        vector<int> ans;
+        queue<int> q;
+        q.push(0);
+	    vis[0]=true;
+	    while(!q.empty())
+	    {
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto it:adj[node])
+	        {
+	            
+	            if(!vis[it])
+	            {
+	                vis[it]=true;
+	                q.push(it);
+	            }
+	        }
+	    }
+        
+        return ans;
+    }
+};
